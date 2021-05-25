8d832ef55e5af6b7ffda692d74da591c393378ed
Caxton Muthoni <githinjicaxton323@gmail.com>
2021-05-25 18:35:04 +0300

    Created auth,and home page

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,75 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">{{ __('HR Login') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+
+                        <div class="form-group row justify-content-center">
+                            <label for="email" class="col-md-11 col-form-label">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-11">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row justify-content-center">
+                            <label for="password" class="col-md-11 col-form-label">{{ __('Password') }}</label>
+
+                            <div class="col-md-11">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <div class="col-md-6 offset-6">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-3 justify-content-end">
+                            <div class="col-md-7">
+
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                @endif
+                            </div>
+                            <div class="col-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/imgs/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/imgs/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/imgs/favicons/favicon-16x16.png">
+    <link rel="manifest" href="/imgs/favicons/site.webmanifest">
+    <link rel="mask-icon" href="/imgs/favicons/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="/imgs/favicons/favicon.ico">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="msapplication-config" content="/imgs/favicons/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+
+    <title>HRM | Human resource management</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet" type="text/css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
+    
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet"> -->
+
+</head>
+
+<body>
+    <div class="welcome" id="app">
+
+        <nav class="nav">
+            <div class="nav-bar__logo nav__logo-1 js--logo-1">
+                <img src="{{asset('/imgs/svgs/HRM.svg')}}" alt="logo" class="nav-bar__logo-image">
+            </div>
+
+            <div class="nav-bar__logo nav__logo-2 js--logo-2">
+                <img src="{{asset('/imgs/svgs/HRM.svg')}}" alt="logo" class="nav__logo-2-image">
+            </div>
+
+            <ul class="nav-items" id="menucontainer">
+                @if (Route::has('login'))
+                @auth
+                <li class="nav-item"><a href="/home" class="nav-link">Dashboard</a></li>
+                <li class="nav-item"><a href="" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();" class="nav-link">Logout</a></li>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+                @else
+                <li class="nav-item"><a href="/login" class="nav-link">Login</a></li>
+                @if (Route::has('register'))
+                <li class="nav-item"><a href="/register" class="nav-link">Register</a></li>
+                @endif
+                @endauth
+                @endif
+            </ul>
+             <i class="fas fa-bars  nav-icon  js--open-menu"></i>
+             <i class="fas fa-window-close nav-small-icon  js--close-menu "></i>
+        </nav>
+
+        <header class="header" id="home">
+            <div class="header__content">
+                <h1 class="heading-1">Human resource management automation</h1>
+                <h4 class="heading-4 heading-4--light">Ensuring best work practices are in place at all times made easier with Human resource management System</h4>
+                <a href="/login" class="btn__home">Get Started Now</a>
+            </div>
+            <div class="header__ilust">
+                <img src="{{asset('imgs/svgs/home.svg')}}" alt="home">
+            </div>
+        </header>
+    </div>
+    <!-- Scripts -->
+    <!-- <script src="{{ asset('js/app.js') }}" defer></script> -->
+   
+</body>
+
+</html>
